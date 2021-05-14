f56eb02ada0eba765104a826ccf1823940a54969
Hawk2272 <email@gmail.com>
2021-05-14 23:39:15 +0300

    sleep

--- Personalized Clock.py
+++ Personalized Clock.py
@@ -51,8 +51,4 @@ while True:
         sleep=Tk()
         sleep.iconbitmap(r'C:\Users\user\Downloads\alarm.ico')
         sleep.geometry("400x200")
-        print()
-        print("Hi world")
-        print("Gaurav Chawla")
-        print()
-        
+    
